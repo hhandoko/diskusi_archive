a4c88df1a264cb2af9ecdfac4c9101644b9af143
Herdy Handoko <herdy.handoko@gmail.com>
2016-03-17 07:07:55 +0800

    Added registration page and split layout into components / partials

--- test/controllers/auth_controller_test.exs
+++ test/controllers/auth_controller_test.exs
@@ -1,5 +1,5 @@
 #
-# File     : endpoint.ex
+# File     : auth_controller_test.exs
 # License  :
 #   The MIT License (MIT)
 #
@@ -23,42 +23,12 @@
 #   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #   THE SOFTWARE.
 #
-defmodule Diskusi.Endpoint do
-  use Phoenix.Endpoint, otp_app: :diskusi
+defmodule Diskusi.AuthControllerTest do
+  use Diskusi.ConnCase
 
-  socket "/socket", Diskusi.UserSocket
-
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phoenix.digest
-  # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/", from: :diskusi, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt humans.txt)
-
-  # Code reloading can be explicitly enabled under the
-  # :code_reloader configuration of your endpoint.
-  if code_reloading? do
-    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
-    plug Phoenix.LiveReloader
-    plug Phoenix.CodeReloader
+  # Registration page should render
+  test "GET /register", %{conn: conn} do
+    conn = get conn, "/register"
+    assert html_response(conn, 200) =~ "Register"
   end
-
-  plug Plug.RequestId
-  plug Plug.Logger
-
-  plug Plug.Parsers,
-    parsers: [:urlencoded, :multipart, :json],
-    pass: ["*/*"],
-    json_decoder: Poison
-
-  plug Plug.MethodOverride
-  plug Plug.Head
-
-  plug Plug.Session,
-    store: :cookie,
-    key: "_diskusi_key",
-    signing_salt: "/GkkfjcQ"
-
-  plug Diskusi.Router
 end
